c09200ca8425842beb1d6aa8d3f94952c4882a7e
Nico Stucki <nstucki@icloud.com>
2024-07-09 17:26:19 +0200

    extractMorseSkeletonParallel only saves the voxels, not the cubes of the morse skeleton

--- src/data_structures.cpp
+++ src/data_structures.cpp
@@ -823,11 +823,16 @@ void MorseComplex::extractMorseSkeletonBelow(const value_t& threshold, const uin
 
 
 void MorseComplex::extractMorseSkeletonInDimBelow(const uint8_t& dim, const value_t& threshold) {
+	vector<vector<index_t>> pixels;
 	if (dim == 0) {
 		for (const Cube& c : C[0]) {
 			if (c.birth >= threshold) { continue; }
 
-			morseSkeletonBelow.insert(c);
+			//morseSkeletonBelow.insert(c);
+			pixels = c.getVertices();
+			for (const vector<index_t>& p : pixels) {
+				morseSkeletonVoxelsBelow.insert(p);
+			}
 		}
 	} else {
 		vector<tuple<Cube, Cube, Cube>> flow;
@@ -838,7 +843,14 @@ void MorseComplex::extractMorseSkeletonInDimBelow(const uint8_t& dim, const valu
 			flow.clear();
 			traverseFlow(c, flow, false);
 			for (const tuple<Cube, Cube, Cube>& t : flow) {
-				if (get<1>(t) != get<2>(t)) { morseSkeletonBelow.insert(get<2>(t)); }
+				if (get<1>(t) != get<2>(t)) { 
+					//morseSkeletonBelow.insert(get<2>(t));
+					pixels = get<2>(t).getVertices();
+					for (const vector<index_t>& p : pixels) {
+						morseSkeletonVoxelsBelow.insert(p);
+					}
+					
+				}
 			}
 		}
 	}
@@ -855,17 +867,17 @@ void MorseComplex::extractMorseSkeletonParallelBelow(const value_t& threshold, c
 								this, dim, threshold));
 	}
 	
-	for (auto& future : futures) {
-        future.wait();
-    }
+	// for (auto& future : futures) {
+    //     future.wait();
+    // }
 
-	vector<vector<index_t>> pixels;
-	for (const Cube& c : morseSkeletonBelow) {
-		pixels = c.getVertices();
-		for (const vector<index_t>& p : pixels) {
-			morseSkeletonVoxelsBelow.insert(p);
-		}
-	}
+	// vector<vector<index_t>> pixels;
+	// for (const Cube& c : morseSkeletonBelow) {
+	// 	pixels = c.getVertices();
+	// 	for (const vector<index_t>& p : pixels) {
+	// 		morseSkeletonVoxelsBelow.insert(p);
+	// 	}
+	// }
 }
 
 
